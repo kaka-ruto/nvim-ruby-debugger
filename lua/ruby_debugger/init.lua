3ac719c4aa3745a6354085796ad32a75707c80c3
Kaka Ruto <kr@kakaruto.com>
2024-09-06 15:56:12 +0100

    Support all rdbg connection methods (TCP, UNIX socket, stdio)

--- lua/ruby_debugger/init.lua
+++ lua/ruby_debugger/init.lua
@@ -14,6 +14,7 @@ M.config = {
 		"-c",
 		"--",
 	},
+	connection_timeout = 5000, -- 5 seconds
 }
 
 local function log(level, message)
@@ -27,34 +28,90 @@ end
 
 local function setup_adapter(dap)
 	dap.adapters.ruby = function(callback, config)
-		local port = config.port or M.config.port
-		local host = config.host or M.config.host
-
-		log("debug", string.format("Setting up Ruby adapter on %s:%d", host, port))
-
-		local opts = {
+		local adapter = {
 			type = "server",
-			host = host,
-			port = port,
+			host = config.host or M.config.host,
+			port = config.port or M.config.port,
+			executable = {
+				command = M.config.debugger_cmd[1],
+				args = vim.tbl_map(function(arg)
+					return arg:gsub("${port}", tostring(config.port or M.config.port))
+						:gsub("${host}", config.host or M.config.host)
+				end, vim.list_extend(vim.list_slice(M.config.debugger_cmd, 2), M.config.rdbg_options)),
+			},
 		}
 
-		if config.request ~= "attach" then
-			local args = vim.deepcopy(M.config.debugger_cmd)
-			vim.list_extend(args, M.config.rdbg_options)
-			for i, arg in ipairs(args) do
-				args[i] = arg:gsub("${port}", tostring(port)):gsub("${host}", host)
-			end
-			if config.script and config.script ~= "" then
-				vim.list_extend(args, { config.script })
-			end
-			opts.executable = {
-				command = args[1],
-				args = vim.list_slice(args, 2),
-			}
-			log("debug", "Launching debugger with command: " .. vim.inspect(opts.executable))
+		if config.request == "attach" then
+			adapter.executable = nil
+		elseif config.rdbg_connection_method == "stdio" then
+			adapter.type = "pipe"
+			adapter.pipe = nil
+		elseif config.rdbg_connection_method == "unix_socket" then
+			adapter.type = "server"
+			adapter.host = nil
+			adapter.port = nil
+			adapter.executable.args = vim.tbl_filter(function(arg)
+				return not arg:match("^%-%-port") and not arg:match("^%-%-host")
+			end, adapter.executable.args)
+			table.insert(adapter.executable.args, "--sock")
+			table.insert(adapter.executable.args, config.unix_socket_path or "/tmp/ruby-debug-sock")
 		end
 
-		callback(opts)
+		if config.script and config.script ~= "" then
+			vim.list_extend(adapter.executable.args, { config.script })
+		end
+
+		log("debug", "Launching debugger with adapter: " .. vim.inspect(adapter))
+
+		local handle
+		local pid_or_err
+		local stdout = vim.loop.new_pipe(false)
+		local stderr = vim.loop.new_pipe(false)
+
+		local function cleanup()
+			stdout:close()
+			stderr:close()
+			if handle then
+				handle:close()
+			end
+		end
+
+		if adapter.executable then
+			handle, pid_or_err = vim.loop.spawn(adapter.executable.command, {
+				args = adapter.executable.args,
+				stdio = { nil, stdout, stderr },
+				detached = true,
+			}, function(code)
+				cleanup()
+				if code ~= 0 then
+					log("error", string.format("Debugger exited with code: %d", code))
+				end
+			end)
+
+			if not handle then
+				log("error", "Failed to start debugger: " .. tostring(pid_or_err))
+				return
+			end
+
+			vim.loop.read_start(stdout, function(err, data)
+				assert(not err, err)
+				if data then
+					log("debug", "Debugger stdout: " .. data)
+				end
+			end)
+
+			vim.loop.read_start(stderr, function(err, data)
+				assert(not err, err)
+				if data then
+					log("error", "Debugger stderr: " .. data)
+				end
+			end)
+		end
+
+		-- Wait for the debugger to be ready
+		vim.defer_fn(function()
+			callback(adapter)
+		end, 100)
 	end
 	log("debug", "Ruby adapter setup complete")
 end
