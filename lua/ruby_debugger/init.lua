0887f784001a0c94dcccfba094bbfdaf58bb20c4
Kaka Ruto <kr@kakaruto.com>
2024-09-06 16:53:48 +0100

    Simplify approach, again

--- lua/ruby_debugger/init.lua
+++ lua/ruby_debugger/init.lua
@@ -4,17 +4,6 @@ M.config = {
 	port = 38698,
 	host = "127.0.0.1",
 	log_level = "info",
-	debugger_cmd = { "bundle", "exec", "rdbg" },
-	rdbg_options = {
-		"--open",
-		"--port",
-		"${port}",
-		"--host",
-		"${host}",
-		"-c",
-		"--",
-	},
-	connection_timeout = 5000, -- 5 seconds
 }
 
 local function log(level, message)
@@ -28,92 +17,26 @@ end
 
 local function setup_adapter(dap)
 	dap.adapters.ruby = function(callback, config)
-		local adapter = {
+		local port = config.port or M.config.port
+		local host = config.host or M.config.host
+
+		local opts = {
 			type = "server",
-			host = config.host or M.config.host,
-			port = config.port or M.config.port,
+			host = host,
+			port = port,
 			executable = {
-				command = M.config.debugger_cmd[1],
-				args = vim.tbl_map(function(arg)
-					return arg:gsub("${port}", tostring(config.port or M.config.port))
-						:gsub("${host}", config.host or M.config.host)
-				end, vim.list_extend(vim.list_slice(M.config.debugger_cmd, 2), M.config.rdbg_options)),
+				command = "bundle",
+				args = { "exec", "rdbg", "-n", "--open", "--port", tostring(port) },
 			},
 		}
 
 		if config.request == "attach" then
-			adapter.executable = nil
-		elseif config.rdbg_connection_method == "stdio" then
-			adapter.type = "pipe"
-			adapter.pipe = nil
-		elseif config.rdbg_connection_method == "unix_socket" then
-			adapter.type = "server"
-			adapter.host = nil
-			adapter.port = nil
-			adapter.executable.args = vim.tbl_filter(function(arg)
-				return not arg:match("^%-%-port") and not arg:match("^%-%-host")
-			end, adapter.executable.args)
-			table.insert(adapter.executable.args, "--sock")
-			table.insert(adapter.executable.args, config.unix_socket_path or "/tmp/ruby-debug-sock")
+			opts.executable = nil
 		end
 
-		if config.script and config.script ~= "" then
-			vim.list_extend(adapter.executable.args, { config.script })
-		end
-
-		log("debug", "Launching debugger with adapter: " .. vim.inspect(adapter))
-
-		local handle
-		local pid_or_err
-		local stdout = vim.loop.new_pipe(false)
-		local stderr = vim.loop.new_pipe(false)
-
-		local function cleanup()
-			stdout:close()
-			stderr:close()
-			if handle then
-				handle:close()
-			end
-		end
-
-		if adapter.executable then
-			handle, pid_or_err = vim.loop.spawn(adapter.executable.command, {
-				args = adapter.executable.args,
-				stdio = { nil, stdout, stderr },
-				detached = true,
-			}, function(code)
-				cleanup()
-				if code ~= 0 then
-					log("error", string.format("Debugger exited with code: %d", code))
-				end
-			end)
-
-			if not handle then
-				log("error", "Failed to start debugger: " .. tostring(pid_or_err))
-				return
-			end
-
-			vim.loop.read_start(stdout, function(err, data)
-				assert(not err, err)
-				if data then
-					log("debug", "Debugger stdout: " .. data)
-				end
-			end)
-
-			vim.loop.read_start(stderr, function(err, data)
-				assert(not err, err)
-				if data then
-					log("error", "Debugger stderr: " .. data)
-				end
-			end)
-		end
-
-		-- Wait for the debugger to be ready
-		vim.defer_fn(function()
-			callback(adapter)
-		end, 100)
+		log("debug", "Adapter options: " .. vim.inspect(opts))
+		callback(opts)
 	end
-	log("debug", "Ruby adapter setup complete")
 end
 
 local function setup_configuration(dap)
@@ -131,27 +54,17 @@ local function setup_configuration(dap)
 			request = "attach",
 			remoteHost = M.config.host,
 			remotePort = M.config.port,
-			remoteWorkspaceRoot = "${workspaceFolder}",
-			cwd = "${workspaceFolder}",
-		},
-		{
-			type = "ruby",
-			name = "Run RSpec (current file)",
-			request = "launch",
-			program = "bundle",
-			programArgs = { "exec", "rspec", "${file}" },
 			cwd = "${workspaceFolder}",
 		},
 		{
 			type = "ruby",
 			name = "Debug Rails server",
 			request = "launch",
-			program = "bundle",
-			programArgs = { "exec", "rails", "server" },
+			program = "bin/rails",
+			programArgs = { "server" },
 			cwd = "${workspaceFolder}",
 		},
 	}
-	log("debug", "Ruby configurations setup complete")
 end
 
 function M.setup(user_config)
