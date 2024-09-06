336fd101bb68acd5bb8d1f1a71d513b88b2ce6b5
Kaka Ruto <kr@kakaruto.com>
2024-09-06 14:04:00 +0100

    Use a simpler adapter setup by using the 'executable' type

--- lua/ruby_debugger/init.lua
+++ lua/ruby_debugger/init.lua
@@ -3,7 +3,7 @@ local M = {}
 M.config = {
 	port = 3001,
 	host = "127.0.0.1",
-	debugger_cmd = "bundle exec rdbg -n --open --port ${port} --host ${host}",
+	debugger_cmd = { "bundle", "exec", "rdbg", "-n", "--open", "--port", "${port}", "--host", "${host}" },
 	log_level = "info",
 }
 
@@ -17,82 +17,27 @@ local function log(level, message)
 end
 
 local function setup_adapter(dap)
-	dap.adapters.ruby = function(callback, config)
-		local port = config.port or M.config.port
-		local host = config.host or M.config.host
-		local debugger_cmd = config.debugger_cmd or M.config.debugger_cmd
-
-		debugger_cmd = debugger_cmd:gsub("${port}", tostring(port)):gsub("${host}", host)
-
-		local stdout = vim.loop.new_pipe(false)
-		local stderr = vim.loop.new_pipe(false)
-
-		local handle, pid_or_err
-		local options = {
-			stdio = { nil, stdout, stderr },
-			args = {},
-			detached = true,
-		}
-
-		handle, pid_or_err = vim.loop.spawn("sh", options, function(code)
-			stdout:close()
-			stderr:close()
-			handle:close()
-			if code ~= 0 then
-				print("ruby debugger exited with code", code)
-			end
-		end)
-
-		if not handle then
-			print("Error running ruby debugger:", pid_or_err)
-			return
-		end
-
-		vim.loop.read_start(stdout, function(err, chunk)
-			assert(not err, err)
-			if chunk then
-				vim.schedule(function()
-					require("dap.repl").append(chunk)
-				end)
-			end
-		end)
-
-		vim.loop.read_start(stderr, function(err, chunk)
-			assert(not err, err)
-			if chunk then
-				vim.schedule(function()
-					require("dap.repl").append(chunk)
-				end)
-			end
-		end)
-
-		-- Write the command to the shell's stdin
-		vim.loop.write(handle, debugger_cmd .. "\n")
-
-		-- Wait for debugger to start
-		vim.defer_fn(function()
-			callback({ type = "server", host = host, port = port })
-		end, 2000)
-
-		print("Ruby debugger started on " .. host .. ":" .. port)
-	end
+	dap.adapters.ruby = {
+		type = "executable",
+		command = "bundle",
+		args = { "exec", "rdbg", "-n", "--open", "--port", "${port}", "--host", "${host}" },
+	}
 end
 
 local function setup_configuration(dap)
 	dap.configurations.ruby = {
 		{
 			type = "ruby",
-			name = "Rails Debug",
+			name = "Debug current file",
+			request = "launch",
+			program = "${file}",
+		},
+		{
+			type = "ruby",
+			name = "Attach to Rails",
 			request = "attach",
 			port = M.config.port,
 			host = M.config.host,
-			debugger_cmd = M.config.debugger_cmd,
-			localfs = true,
-			options = {
-				source_filetype = "ruby",
-			},
-			cwd = vim.fn.getcwd(),
-			remoteWorkspaceRoot = vim.fn.getcwd(),
 		},
 	}
 end
