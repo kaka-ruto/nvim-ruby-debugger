30ccfc3e3276b29abc5bd7f6a587e74ae070e9fc
Kaka Ruto <kr@kakaruto.com>
2024-09-06 15:49:52 +0100

    Improve the separation

--- lua/ruby_debugger/init.lua
+++ lua/ruby_debugger/init.lua
@@ -27,18 +27,22 @@ end
 
 local function setup_adapter(dap)
 	dap.adapters.ruby = function(callback, config)
+		local port = config.port or M.config.port
+		local host = config.host or M.config.host
+
+		log("debug", string.format("Setting up Ruby adapter on %s:%d", host, port))
+
 		local opts = {
 			type = "server",
-			host = M.config.host,
-			port = M.config.port,
+			host = host,
+			port = port,
 		}
-		if config.request == "attach" then
-			opts.executable = nil
-		else
+
+		if config.request ~= "attach" then
 			local args = vim.deepcopy(M.config.debugger_cmd)
 			vim.list_extend(args, M.config.rdbg_options)
 			for i, arg in ipairs(args) do
-				args[i] = arg:gsub("${port}", tostring(M.config.port)):gsub("${host}", M.config.host)
+				args[i] = arg:gsub("${port}", tostring(port)):gsub("${host}", host)
 			end
 			if config.script and config.script ~= "" then
 				vim.list_extend(args, { config.script })
@@ -47,7 +51,9 @@ local function setup_adapter(dap)
 				command = args[1],
 				args = vim.list_slice(args, 2),
 			}
+			log("debug", "Launching debugger with command: " .. vim.inspect(opts.executable))
 		end
+
 		callback(opts)
 	end
 	log("debug", "Ruby adapter setup complete")
@@ -60,6 +66,7 @@ local function setup_configuration(dap)
 			name = "Debug current file",
 			request = "launch",
 			program = "${file}",
+			cwd = "${workspaceFolder}",
 		},
 		{
 			type = "ruby",
@@ -68,20 +75,23 @@ local function setup_configuration(dap)
 			remoteHost = M.config.host,
 			remotePort = M.config.port,
 			remoteWorkspaceRoot = "${workspaceFolder}",
+			cwd = "${workspaceFolder}",
 		},
 		{
 			type = "ruby",
 			name = "Run RSpec (current file)",
 			request = "launch",
-			program = "rspec",
-			args = { "${file}" },
+			program = "bundle",
+			programArgs = { "exec", "rspec", "${file}" },
+			cwd = "${workspaceFolder}",
 		},
 		{
 			type = "ruby",
 			name = "Debug Rails server",
 			request = "launch",
-			program = "rails",
-			args = { "server" },
+			program = "bundle",
+			programArgs = { "exec", "rails", "server" },
+			cwd = "${workspaceFolder}",
 		},
 	}
 	log("debug", "Ruby configurations setup complete")
